beaade8bb74c1dac3824c7db5009647cb689a94b
Hafiz <mhafiz.shaifullnaim@petronas.com.my>
2024-01-22 19:44:30 +0800

    First initial commit

---
+++ fizzbuzz.js
@@ -0,0 +1,21 @@
+// Function to implement the FizzBuzz logic
+function fizzBuzz(n) {
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("FizzBuzz");
+        } else if (i % 3 === 0) {
+            console.log("Fizz");
+        } else if (i % 5 === 0) {
+            console.log("Buzz");
+        } else {
+            console.log(i);
+        }
+    }
+}
+
+// Read the value of n in the command line 
+const n = process.argv[2] || 15;
+
+
+// Run the function with the value of n
+fizzBuzz(Number(n));
